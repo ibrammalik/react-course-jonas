058fbd9dce364b086ffe485c2c9a8cec99c0f22f
ibrammalik <malikmaul1173@gmail.com>
2024-07-27 11:57:49 +0700

    create second state to count advice that has been read

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -2,19 +2,22 @@ import { useState } from "react";
 
 export default function App() {
   const [advice, setAdvice] = useState("Click button bellow to get advice.");
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((count) => count + 1);
   }
 
   return (
     <div>
-      <h1>Hello World!</h1>
-      <button>Get Advice</button>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get Advice</button>
+      <p>
+        You have read <strong>{count}</strong> pieces of advice
+      </p>
     </div>
   );
 }
