c17d4e255d5985c357d7bb3ecd041bb50a859988
ibrammalik <malikmaul1173@gmail.com>
2024-07-27 11:55:15 +0700

    create state advice and function to get advice

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 export default function App() {
+  const [advice, setAdvice] = useState("Click button bellow to get advice.");
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
+
   return (
     <div>
       <h1>Hello World!</h1>
       <button>Get Advice</button>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
 }
